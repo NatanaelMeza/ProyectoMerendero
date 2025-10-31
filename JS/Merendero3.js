d0c12aec5cb42462d1b00e032aa0a7b4d3799ff4
luciaamarquez <luciaamarquez222@gmail.com>
2025-10-31 08:51:39 -0300

    Elimino una animación que se repite en Merendero3.js

--- JS/Merendero3.js
+++ JS/Merendero3.js
@@ -1,26 +1,3 @@
-            document.addEventListener("DOMContentLoaded", () =>{
-
-            //  Mensaje cuando cargas la página
-            console.log("Bienvenida a la página del merendero Los Pequeños Gigantes");
-            alert("¡Hola! Bienvenida/o al merendero Los Pequeños Gigantes");
-    
-            const elementos=document.querySelectorAll('.animar-secuencia');
-    
-            function mostrarSecuencia(){
-                const alturaVentana=window.innerHeight;
-    
-                elementos.forEach((el, index)=>{
-                    const distancia=el.getBoundingClientRect().top;
-                    if(distancia<alturaVentana-150&&!el.classList.contains('visible')){
-                        setTimeout(() =>{
-                            el.classList.add('visible');
-                        }, index*220);
-                    }
-                });
-            }
-            window.addEventListener('scroll', mostrarSecuencia);
-            mostrarSecuencia();
-            });
   document.addEventListener("DOMContentLoaded", () =>{
 
             //  Mensaje cuando cargas la página
